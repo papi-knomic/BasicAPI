2f4bf45ec01dbbbf7caca43b75eb1f88d52297af
Samson Moses <knomic10@gmail.com>
2023-02-23 16:08:02 +0100

    Added json middleware to force all response to json

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Traits\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['json']], function () {
+    Route::get('/', function () {
+        return Response::successResponse('Welcome to Basic API');
+    });
 });
+
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
