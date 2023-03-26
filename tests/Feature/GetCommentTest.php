2c7ae0752956cf28d3afc9dde50c10e820a0f4f6
Samson Moses <knomic10@gmail.com>
2023-03-26 20:05:40 +0100

    Created test to get comments for post and also single comments

--- tests/Feature/GetCommentTest.php
+++ tests/Feature/GetCommentTest.php
@@ -2,21 +2,62 @@
 
 namespace Tests\Feature;
 
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class GetCommentTest extends TestCase
 {
+    protected $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = $this->createUser();
+    }
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_get_single_comment_endpoint()
     {
-        $response = $this->get('/');
+        $comment = Comment::factory()->create();
+        $response = $this->get(route('comment.show', ['comment' => $comment]));
 
-        $response->assertStatus(200);
+        $response->assertStatus(self::HTTP_REDIRECT);
+    }
+
+    public function test_get_single_comment_success()
+    {
+        $this->be($this->user);
+        $comment = Comment::factory()->create();
+        $response = $this->get(route('comment.show', ['comment' => $comment]));
+
+        $response->assertStatus(self::HTTP_OK);
+    }
+
+    public function test_get_post_comments_endpoint()
+    {
+        $post = Post::factory()->create();
+        $response = $this->get(route('comment.index', ['post' => $post]));
+
+        $response->assertStatus(self::HTTP_REDIRECT);
+    }
+
+
+    public function test_get_post_comments_success()
+    {
+        $this->be($this->user);
+        $post = Post::factory()->create();
+        Comment::factory()->count(5)->create(['post_id' => $post->id]);
+        $response = $this->get(route('comment.index', ['post' => $post]));
+
+
+        $response->assertStatus(self::HTTP_OK);
     }
 }
